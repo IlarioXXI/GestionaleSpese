4c2c3de222532c03628981c06ba774aa4aecf19c
Ilario <ilario01donhghia@hotmail.com>
2024-11-11 11:21:05 +0100

    Initial commit

---
+++ src/main/java/com/apuliadigitalmaker/gestionalespese/GestionaleSpeseApplication.java
@@ -0,0 +1,13 @@
+package com.apuliadigitalmaker.gestionalespese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleSpeseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionaleSpeseApplication.class, args);
+    }
+
+}
