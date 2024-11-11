2a3b00a4894c06dcc6d13c7f0c22547e644331bb
Ilario <ilario01donhghia@hotmail.com>
2024-11-11 17:02:46 +0100

    account quasi finito

---
+++ src/main/java/com/apuliadigitalmaker/gestionalespese/category/Category.java
@@ -0,0 +1,93 @@
+package com.apuliadigitalmaker.gestionalespese.category;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "categories",schema = "gestionale_spese")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "category_name", nullable = false)
+    private String categoryName;
+
+    @Column(name = "expense_earning", nullable = false)
+    private Byte expenseEarning;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "created", nullable = false)
+    private Instant created;
+
+    @ColumnDefault("current_timestamp()")
+    @Column(name = "updated")
+    private Instant updated;
+
+    @Column(name = "deleted")
+    private Instant deleted;
+
+    @PrePersist
+    protected void onCreate() {
+        created = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = Instant.now();
+    }
+
+    public void softDelete() {
+        this.deleted = Instant.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Byte getExpenseEarning(){
+        return expenseEarning;
+    }
+
+    public void setExpenseEarning(Byte expenseEarning){
+        this.expenseEarning = expenseEarning;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+
+    public Instant getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Instant deleted) {
+        this.deleted = deleted;
+    }
+
+
+}
